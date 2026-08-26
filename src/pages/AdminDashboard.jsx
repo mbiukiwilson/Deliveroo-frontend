a1fafe852890d3709467a6e3cc3d2781fef83250
Ccode-400 <stanleymngige@gmail.com>
2026-08-26 04:48:15 +0300

    added export default AdminDashboard function in AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -2,3 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import api from "../api";
 import { t } from "../i18n";
+
+export default function AdminDashboard() {
+  const user = useSelector((state) => state.auth.user);
+  const { language } = useSelector((state) => state.preferences);
+  const [parcels, setParcels] = useState([]);
+  const [error, setError] = useState("");
+  const watchId = useRef(null);
+  const activeParcel = useRef(null);
